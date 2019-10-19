6ce07b2ced1891002cd687f1f81e9ee42c02b7c2
kimkwanghoon <rhkdgns9489@gmail.com>
2019-10-19 11:28:57 +0900

    #1.1 로딩 아이콘 다운, Firebase 연동(원격 서비스 값 활성화).

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
 # Ahn Talk
 - Firebase를 활용하여 Android 채팅톡 만들기.
+> API 23: Android 6.0 (Marshmallow)
+> 아이콘 파일 다운로드: https://www.iconfinder.com에서 상단의 SearchBar 검색 후 [free]선택하여 다운로드
+
+> Firebase 연동하기 - 소스코드 참고 https://firebase.google.com/docs/remote-config/use-config-android?authuser=0
+
+> * 1. https://console.firebase.google.com Firebase 생성하기 프로젝트 명 'AhnTalk'.
+> * 2. 앱 등록하기 - https://console.firebase.google.com/project/ahntalk/overview
+> > > 구성파일 다운로드 - google-service.json 다운로드 후 app/google-service.json처럼 app폴더 안에 저장.
+> > > Firebase SDK 추가.
+> * 3. 안드로이드 스튜디오 [Tools] - Firebase 클릭 -> Set up Firebase Remote Config.
+> * 4. Connect your app to Firebase클릭.
+> * 5. Add Remote Config to your app 클릭.
+> * 6. Set in-app default parameter values코드 복사 후 app/res/xml/default_config 생성하여 복사하기.
+> * 7. https://console.firebase.google.com/project/ahntalk/config 에서 원격 데이터 Key-value 저장하기.
+> > > 매개변수(1) splash_background: "#ed95ff"
+> > > 매개변수(2) splash_cap: true // true - 서버점검중 메시지 확인 이후 종료되는지 확인해보고 false로 설정 할 것.
+> > > 매개변수(3) splash_background: "서버 점검중입니다."
+> * 8. [변경사항 게시] 클릭.
+
+> Design 추가
+> build.gralde(Module: app)에 implementation 'com.google.android.material:material:1.0.0' 추가.
 
 ## Todo
 - [x] 'Ahn' Project 생성 및 MainActivity -> SplashActivity 이름변경.
+- [] 로딩 아이콘 파일 다운로드 및 Firebase연동하기.
--- app/src/main/java/com/example/ahntalk/SplashActivity.java
+++ app/src/main/java/com/example/ahntalk/SplashActivity.java
@@ -1,14 +1,101 @@
 package com.example.ahntalk;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
 
 public class SplashActivity extends AppCompatActivity {
+    private LinearLayout linearLayout;
+    private FirebaseRemoteConfig mFirebaseRemoteConfig;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        linearLayout = (LinearLayout) findViewById(R.id.splashactivity_linearlayout);
+
+        /**
+         *  (1) Android에서 Firebase 원격 구성시작하기. - 원격구성 객체 가져오기
+         *
+         *  소스코드 참고
+         *  https://firebase.google.com/docs/remote-config/use-config-android?authuser=0
+         *
+         */
+        mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+        FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder()
+                .setDeveloperModeEnabled(BuildConfig.DEBUG)
+                .build();
+        mFirebaseRemoteConfig.setConfigSettings(configSettings);
+
+        /**
+         *  (2) 인앱 매개변수 기본값 설정
+         *
+         *  res/xml 폴더에 저장된 XML 리소스 파일을 사용하여 매개변수 이름과 매개변수 기본값 집합을 정의.
+         *  원격 구성 빠른 시작 샘플 앱은 XML 파일을 사용하여 매개변수 이름과 기본값을 정의.
+         *
+         */
+        mFirebaseRemoteConfig.setDefaults(R.xml.default_config);
+
+        /**
+         *  (3) 서비스에서 값을 가져와서 활성화시키기
+         *
+         *  원격구성 서비스에서 데이터를 가져와서 활성화.
+         *  fetch() 메서드를 통해서 매개변수값 가져오기.
+         *
+         */
+        mFirebaseRemoteConfig.fetch(0)
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            mFirebaseRemoteConfig.activateFetched();
+                        } else {
+
+                        }
+                        displayMessage();
+                    }
+                });
+    }
+
+    /**
+     *  원격 데이터 가져온뒤, 해당 값 설정하기.
+     *
+     *  원격저장소에 해당 키 값에 대해서
+     *  미리 값이 정의되어 있어야 올바르게 가져온다.
+     *
+     */
+    void displayMessage() {
+        String splash_background = mFirebaseRemoteConfig.getString("splash_background");
+        boolean caps = mFirebaseRemoteConfig.getBoolean("splash_message_caps");
+        String splash_message = mFirebaseRemoteConfig.getString("splash_message");
+
+        linearLayout.setBackgroundColor(Color.parseColor(splash_background));
+
+        /**
+         *  caps: 서버점검중인 경우 (boolean)
+         *
+         *  true - finish(); 종료.
+         */
+        if(caps) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage(splash_message).setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                }
+            });
+            builder.create().show();
+        }
     }
 }
